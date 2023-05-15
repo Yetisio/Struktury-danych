397ac349d5695725b5d4f665011aeae53f3d8948
Yetisio <jakub.piekarek@wp.pl>
2023-05-15 15:15:52 +0200

    15.05 zmiany
    3 struktury danych

---
+++ Algo_Prima_Macierz_Sasiedztwa/Algorytm Prima i struktury.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+int const V = 9;
+// Funkcja zwracająca minimalną wagę krawędzi
+int minKey(int key[], bool mstSet[], int V)
+{
+    // Inicjalizuj minimum jako nieskończony
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < V; v++)
+        if (mstSet[v] == false && key[v] < min)
+            min = key[v], min_index = v;
+
+    return min_index;
+}
+
+// Funkcja wyświetlająca wynik
+void printMST(int parent[], int n, int graph[][9])
+{
+    cout << "Krawedz    Waga\n";
+    for (int i = 1; i < n; i++)
+        cout << parent[i] << " - " << i << "    " << graph[i][parent[i]] << "\n";
+}
+
+// Funkcja generująca MST dla grafu podanego w postaci macierzy sąsiedztwa
+int primMST(int graph[][V])
+{
+    int parent[V]; // tablica przechowująca rozpięte drzewo
+    int key[V];   // tablica przechowująca połączone krawędzie
+    bool mstSet[V];  // tablica wskazująca, czy wierzchołek należy już do MST
+
+    // Inicjalizuj wszystkie klucze jako nieskończone i mstSet[] jako false
+    for (int i = 0; i < V; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
+
+    // Ustaw klucz na 0 dla pierwszego wierzchołka
+    key[0] = 0;
+    parent[0] = -1; // To wierzchołek jest korzeniem MST
+
+    // Przeglądaj wszystkie wierzchołki
+    for (int count = 0; count < V - 1; count++)
+    {
+        // Wybierz najmniejszy klucz wierzchołka, który nie należy jeszcze do MST
+        int u = minKey(key, mstSet, V);
+
+        // Dodaj wierzchołek do MST
+        mstSet[u] = true;
+
+        // Aktualizuj klucze dla wszystkich sąsiadów wybranego wierzchołka.
+        // Jeśli krawędź z wierzchołkiem u do sąsiada v jest mniejsza od obecnego
+        // klucza dla v, to zaktualizuj klucz dla sąsiada.
+        for (int v = 0; v < V; v++)
+        {
+            // graph[u][v] nie jest równy 0
+            // mstSet[v] jest false
+            // graph[u][v] jest mniejsze od dotychczasowego klucza dla wierzchołka v
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
+            {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    // Wyświetl wynik
+   // printMST(parent, V, graph);
+    // Obliczamy sumę wag dla MST
+    int sum = 0;
+    for (int i = 1; i < V; i++)
+        sum += key[i];
+
+    return sum;
+}
+// Driver program
+int main()
+{
+    // Macierz sąsiedztwa dla grafu w postaci tablicy dwuwymiarowej
+    int V = 9;
+    int graph[9][9] = { {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                       {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                       {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                       {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                       {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                       {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                       {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                       {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                       {0, 0, 2, 0, 0, 0, 6, 7, 0}
+    };
+
+    // Wywołanie algorytmu
+    cout<<primMST(graph);
+
+    return 0;
+}
+
---
+++ Algo_Prima_struktura_drzewa/Algo_Prima_struktura_drzewa.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+int const V = 9;
+// Struktura drzewa
+struct Tree
+{
+    int rank;
+    int parent;
+};
+
+// Funkcja tworząca zbiór z wierzchołka i
+int makeSet(int i, vector<Tree>& sets)
+{
+    Tree newSet;
+    newSet.rank = 0;
+    newSet.parent = i;
+
+    sets.push_back(newSet);
+    return sets.size() - 1;
+}
+
+// Funkcja zwracająca reprezentanta zbioru wierzchołka i
+int findSet(int i, vector<Tree>& sets)
+{
+    if (sets[i].parent != i)
+        sets[i].parent = findSet(sets[i].parent, sets);
+
+    return sets[i].parent;
+}
+
+// Funkcja łącząca 2 zbiory
+void unionSet(int i, int j, vector<Tree>& sets)
+{
+    int i_id = findSet(i, sets);
+    int j_id = findSet(j, sets);
+
+    if (i_id == j_id)
+        return;
+
+    if (sets[i_id].rank > sets[j_id].rank)
+        sets[j_id].parent = i_id;
+    else
+    {
+        sets[i_id].parent = j_id;
+        if (sets[i_id].rank == sets[j_id].rank)
+            sets[j_id].rank++;
+    }
+}
+
+// Funkcja wyznaczająca minimalne drzewo rozpinające (MST)
+// dla grafu nieskierowanego o wagach
+int PrimMST(vector<pair<int, int> > adj[])
+{
+    vector<pair<int, pair<int, int> > > edges;
+
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < adj[i].size(); j++)
+        {
+            int v = adj[i][j].first;
+            int weight = adj[i][j].second;
+            edges.push_back(make_pair(weight, make_pair(i, v)));
+        }
+    }
+
+    sort(edges.begin(), edges.end());
+
+    vector<Tree> sets(V);
+    for (int i = 0; i < V; i++)
+        makeSet(i, sets);
+
+    int sumWeight = 0;
+    vector<pair<int, pair<int, int> > >::iterator it;
+    for (it = edges.begin(); it != edges.end(); it++)
+    {
+        int u = it->second.first;
+        int v = it->second.second;
+
+        int set_u = findSet(u, sets);
+        int set_v = findSet(v, sets);
+
+        if (set_u != set_v)
+        {
+            sumWeight += it->first;
+            unionSet(set_u, set_v, sets);
+        }
+    }
+
+    return sumWeight;
+}
+
+void addEdge(vector<pair<int, int> > adj[], int u, int v, int w)
+{
+    adj[u].push_back(make_pair(v, w));
+    adj[v].push_back(make_pair(u, w));
+}
+int main()
+{
+   
+    vector<pair<int, int> > adj[V];
+
+    // dodaj krawędzie
+    addEdge(adj, 0, 1, 4);
+    addEdge(adj, 0, 7, 8);
+    addEdge(adj, 1, 2, 8);
+    addEdge(adj, 1, 7, 11);
+    addEdge(adj, 2, 3, 7);
+    addEdge(adj, 2, 8, 2);
+    addEdge(adj, 2, 5, 4);
+    addEdge(adj, 3, 4, 9);
+    addEdge(adj, 3, 5, 14);
+    addEdge(adj, 4, 5, 10);
+    addEdge(adj, 5, 6, 2);
+    addEdge(adj, 6, 7, 1);
+    addEdge(adj, 6, 8, 6);
+    addEdge(adj, 7, 8, 7);
+
+    cout<<PrimMST(adj);
+
+    return 0;
+}
